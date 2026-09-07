1ba9b388a5a2ddec8d2efcbf8f5e76ce47b7cc96
JussiPaalanen <jusspaal@metropolia.fi>
2026-09-07 09:56:53 +0300

    peliprojekti

--- Peliprojekti/peliprojekti1.py
+++ Peliprojekti/peliprojekti1.py
@@ -1,5 +1,48 @@
 nimi = input("Mikä on pelinimesi:\n")
-ikä = input("Mikä on ikäsi:\n")
+ikä = int(input("Mikä on ikäsi:\n"))
 
 print(f"{nimi}")
 print(f"{ikä}")
+
+while ikä > 12:
+    print(f"Tervetuloa")
+    break
+
+    
+else:
+    print(f"Olet Alaikäinen")
+
+def nayta_valikko():
+    print("\n--- PÄÄVALIKKO ---")
+    print("1. Peli1")
+    print("2. Peli2")
+    print("3. Peli3")
+    print("0. Lopeta")
+
+def main():
+    while True:
+        nayta_valikko()
+        valinta = input("Valitse: ").strip()
+
+        if valinta == "1":
+            print(f"Ladataan peli1...")
+            break
+        elif valinta == "2":
+            print(f"Ladataan peli2...")
+            break
+        elif valinta == "3":
+            print(f"Ladataan peli3...")
+            break
+        elif valinta == "0":
+            print(f"Lopetetaan.")
+            break
+        else:
+            print(f"Virheellinen valinta.")
+            nayta_valikko()
+
+main()
+
+
+
+
+
