9c48526ff071441f1b86b10b0489781fafee0e0b
JussiPaalanen <jusspaal@metropolia.fi>
2026-09-20 14:08:33 +0300

    tehtävä 3

--- mod09/python3.py
+++ mod09/python3.py
@@ -1,30 +1,38 @@
+import random
+ 
+ 
 class Auto:
     def __init__(self, rekisteritunnus, huippunopeus):
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
         self.nopeus = 0
         self.matka = 0
-
+ 
     def kiihdytä(self, kmh):
-        self.nopeus = self.nopeus + kmh    
+        self.nopeus = self.nopeus + kmh
         if self.nopeus < 0:
             self.nopeus = 0
         if self.nopeus > self.huippunopeus:
             self.nopeus = self.huippunopeus
-
+ 
     def kulje(self, tunti):
-        self.matka = self.matka + self.nopeus * tunti  
+        self.matka = self.matka + self.nopeus * tunti
+ 
+ 
+autot = []
+for i in range(1, 11):
+    rekisteritunnus = f"ABC-{i}"
+    huippunopeus = random.randint(100, 200)
+    autot.append(Auto(rekisteritunnus, huippunopeus))
+ 
+while not any(auto.matka >= 10000 for auto in autot):
+    for auto in autot:
+        muutos = random.randint(-10, 15)
+        auto.kiihdytä(muutos)
+        auto.kulje(1)
+ 
 
-   
-a1 = Auto("ABC-123", 142)
-
-a1.kiihdytä(60)
-
-a1.kulje(1.5)
-
-
-print(f"Auton nopeus = {a1.nopeus}km/h\nMatka ajettu: {a1.matka}")
-
-#a1.kiihdytä(-200)
-
-#print(f"Äkki jarrutuksen jälkeen nopeus on = {a1.nopeus}km/h")
+print(f"{'Rekisteritunnus':<16}{'Huippunopeus':<15}{'Nopeus':<10}{'Matka':<10}")
+for auto in autot:
+    print(f"{auto.rekisteritunnus:<16}{auto.huippunopeus:<15}{auto.nopeus:<10}{auto.matka:<10.0f}")
+ 
